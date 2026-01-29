6516464727e7533f202749eed458c31b13e113ed
DearPratipal <kumarpratipal123@gmail.com>
2026-01-30 00:46:43 +0530

    Update and add R resources

---
+++ BCA-602_R_Programming_Lab - Dr. Varun Gupta/R_ProgramResourceScript/ProgramNo_01.R
@@ -0,0 +1,34 @@
+# Directions / Steps to Perform the Program
+# Step 1: Install R
+# Open a web browser.
+# Go to CRAN R official website.
+# Download the latest version of R according to your operating system (Windows / macOS / Linux).
+# Run the installer and complete the installation using default settings.
+
+# Step 2: Install RStudio
+# Visit the RStudio official website.
+# Download RStudio Desktop (Free version).
+# Install RStudio after R installation is completed.
+
+# Step 3: Open RStudio
+# Launch RStudio.
+# Observe the main components:
+# Console – Executes R commands
+# Source – Write R scripts
+# Environment – Shows variables and objects
+# Plots / Files / Help – Displays outputs and help
+
+# Step 4: Run Basic R Commands
+# In the Console, type:
+# 2 + 3
+# Press Enter to see the output.
+
+# Step 5: Write and Execute the Program
+# Click File → New File → R Script
+# Type the following program.
+# Save the file.
+# Click Run or press Ctrl + Enter.
+
+# Program to display Welcome message
+print("Welcome to R Programming")
+# Output: [1] "Welcome to R Programming"
--- BCA-602_R_Programming_Lab - Dr. Varun Gupta/R_ProgramResourceScript/TestFileR.r
+++ BCA-602_R_Programming_Lab - Dr. Varun Gupta/R_ProgramResourceScript/TestFileR.r
@@ -1,5 +1,5 @@
+# Create a prgram to take two numbers as input and perform basic arithmetic operations from user.
 # Program to perform arithmetic operations on two numbers
-
 # Assign values to variables
 a <- 10
 b <- 5
@@ -15,9 +15,11 @@ cat("Sum =", sum, "\n")
 cat("Difference =", difference, "\n")
 cat("Product =", product, "\n")
 cat("Division =", division, "\n")
+msg <- paste(# Output:
+"Sum = 15",
+"Difference = 5",
+"Product = 50",
+"Division = 2", sep = ", ")
 
-# Output:
-# Sum = 15
-# Difference = 5
-# Product = 50
-# Division = 2
+cat("Sum = 15", " ", "Difference = 5", "Product = 50", "Division = 2", "\n")
+msg
